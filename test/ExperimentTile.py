c3232c8fbd42b2c502551369493d688f807809f4
cgxuvector <xu.cheng@husky.neu.edu>
2020-04-27 23:28:12 -0400

    fix-save-fn-bug-and-policy-fn

--- test/ExperimentTile.py
+++ test/ExperimentTile.py
@@ -213,9 +213,9 @@ class Experiment(object):
 
                 # evaluate the current policy
                 if episode_idx % 100 == 0:
-                    # evaluate the current policy by interaction 
+                    # evaluate the current policy by interaction
+                    self.policy_evaluate()
                     with torch.no_grad():
-                        self.policy_evaluate()
                         # save the model
                         model_save_path = os.path.join(self.save_dir, self.model_name) + f"_{episode_idx}.pt"
                         torch.save(self.agent.policy_net.state_dict(), model_save_path)
@@ -331,8 +331,8 @@ class Experiment(object):
                 dones_buffer = []  # reset the dones buffer
 
                 if episode_idx % 100 == 0:
+                    self.policy_evaluate()
                     with torch.no_grad():
-                        self.policy_evaluate()
                         model_save_path = os.path.join(self.save_dir, self.model_name) + f"_{episode_idx}.pt"
                         torch.save(self.agent.policy_net.state_dict(), model_save_path)
 
@@ -651,8 +651,9 @@ class Experiment(object):
         rewards = []
         for i in range(self.max_steps_per_episode):
             # get one action
-            action = self.agent.get_action(self.toTensor(state)) if not self.use_goal else \
-                        self.agent.get_action(self.toTensor(state), self.toTensor(goal))
+            with torch.no_grad():
+                action = self.agent.get_action(self.toTensor(state)) if not self.use_goal else \
+                            self.agent.get_action(self.toTensor(state), self.toTensor(goal))
 
             # step in the environment
             next_state, reward, done, dist, trans, _, _ = self.env.step(action)
